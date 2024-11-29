2c66e207781b1ee6b8b311805153751fc9467398
volse <volzjohannes@t-online.de>
2024-11-29 15:12:03 +0100

    reiteration, forgot c Syntax

--- 2.0/src/calculation_of_runtime/calculation_of_runtime.ino
+++ 2.0/src/calculation_of_runtime/calculation_of_runtime.ino
@@ -2,17 +2,22 @@ const int buttonPin = 2;
 const int pumpPins[] = {3, 5, 6};
 const unsigned long pumpTime = 1000; //in ms
 
-void setup() {
-  pinMode(buttonPin, INPUT_PULLUP);//pin setup for button needs to pass 5v to pin 2
-  for (int i = 0; i < 3; i++) {
-    pinMode(pumpPins[i], OUTPUT);// pin setup for pumps
-  }
-  void pump(int pin, unsigned long time) { // pump function 
+
+
+ void pump(int pin, unsigned long time) { // pump function 
 
     digitalWrite(pin, HIGH); //pump on
     delay(time);
     digitalWrite(pin, LOW); //pump off
   }
+
+
+void setup() {
+  pinMode(buttonPin, INPUT_PULLUP);//pin setup for button needs to pass 5v to pin 2
+  for (int i = 0; i < 3; i++) {
+    pinMode(pumpPins[i], OUTPUT);// pin setup for pumps
+  }
+ 
 }
 
 void loop() {
